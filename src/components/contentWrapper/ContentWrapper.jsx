4d35960940c1a0231b9c4b85c2948ec69867a082
pankaj114425 <115251359+pankaj114425@users.noreply.github.com>
2023-09-17 15:23:16 +0530

    movix app using react code

---
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./style.scss";
+
+const ContentWrapper = ({ children }) => {
+    return <div className="contentWrapper">{children}</div>;
+};
+
+export default ContentWrapper;
+
+
+//this component is used for align the content in center 
